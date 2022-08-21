07e15e86c2f214928889ac2a5f2562968744a3f1
Daniel <daniel.kozarski@gmail.com>
2022-08-21 19:02:56 +0200

    Initial commit

---
+++ src/main/java/com/sam/bomberman/BombermanApplication.java
@@ -0,0 +1,13 @@
+package com.sam.bomberman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BombermanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BombermanApplication.class, args);
+    }
+
+}
